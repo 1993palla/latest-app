47712db37b3aaa3ee47c81f5a0d205bd0707fe1f
1993palla <49398797+1993palla@users.noreply.github.com>
2021-08-23 19:04:33 +0530

    table added

--- web.py
+++ web.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def index():
-  return render_template('home.html')
+  items=[
+  {'id':1, 'name':'Ashok', 'barcode':'1232453453','phone':'9951558825','price':500},
+      {'id':2, 'name':'Amani', 'barcode':'1232453453','phone':'9951558825','price':500},
+      {'id':3, 'name':'Arun', 'barcode':'1232453453','phone':'9951558825','price':500}
+  ]
+  return render_template('home.html',items_sec=items)
 @app.route('/about')
-def about_page():
-  return render_template('about.html',item_name='Phone' )
+def about_page():  
+  return render_template('about.html')

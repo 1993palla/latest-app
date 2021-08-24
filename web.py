66d8aa6b1f43fcf9c57e9922e00cdc87f7362549
1993palla <49398797+1993palla@users.noreply.github.com>
2021-08-24 09:28:03 +0530

    Hew

--- web.py
+++ web.py
@@ -3,12 +3,12 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def index():
-  items=[
-  {'id':1, 'name':'Ashok', 'phone':'9951558825','email':'pashok8825@gmail.com','dob':'24-Mar-1993'},
-      {'id':2, 'name':'Amani', 'phone':'9951554425','email':'amani.palla18@gmail.com','dob':'24-Mar-1993'},
-      {'id':3, 'name':'Arun', 'phone':'9951338825','email':'arunkumar5@gmail.com','dob':'24-Mar-1993'}
-  ]
-  return render_template('home.html',items_sec=items)
+  return render_template('home.html')
 @app.route('/about')
 def about_page():  
-  return render_template('user_detail.html')
+    items=[
+              {'id':1, 'name':'Ashok', 'phone':'9951558825','email':'pashok8825@gmail.com','dob':'24-Mar-1993'},
+              {'id':2, 'name':'Amani', 'phone':'9951554425','email':'amani.palla18@gmail.com','dob':'24-Mar-1993'},
+              {'id':3, 'name':'Arun', 'phone':'9951338825','email':'arunkumar5@gmail.com','dob':'24-Mar-1993'}
+          ]
+  return render_template('user_detail.html', items_sec=items)

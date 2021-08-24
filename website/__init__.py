d0f869cf47333c4aa660be3794c219cd25e5bff9
1993palla <49398797+1993palla@users.noreply.github.com>
2021-08-24 18:24:58 +0530

    Modified

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from os import path
-from flask_login.login_manager import LoginManager
-from flask_sqlalchemy import SQLAlchemy
+# from flask_login.login_manager import LoginManager
+# from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
-DB_NAME = "database.db"
+# db = SQLAlchemy()
+# DB_NAME = "database.db"
 def create_app():
     app = Flask (__name__)
-    app.config['SECRET_KEY'] = 'asfdha hkjahsdk'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+#     app.config['SECRET_KEY'] = 'asfdha hkjahsdk'
+#     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
     from .views import views
     from .auth import auth

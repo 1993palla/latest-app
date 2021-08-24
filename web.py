d3750adf538cdb1c5fb7906d053eaf084d05aae2
1993palla <49398797+1993palla@users.noreply.github.com>
2021-08-24 12:36:27 +0530

    Update web.py

--- web.py
+++ web.py
@@ -6,9 +6,5 @@ def index():
   return render_template('home.html')
 @app.route('/about')
 def about_page():  
-    items=[
-              {'id':1, 'name':'Ashok', 'phone':'9951558825','email':'pashok8825@gmail.com','dob':'24-Mar-1993'},
-              {'id':2, 'name':'Amani', 'phone':'9951554425','email':'amani.palla18@gmail.com','dob':'24-Mar-1993'},
-              {'id':3, 'name':'Arun', 'phone':'9951338825','email':'arunkumar5@gmail.com','dob':'24-Mar-1993'}
-          ]
+    items=[{'id':1, 'name':'Ashok', 'phone':'9951558825','email':'pashok8825@gmail.com','dob':'24-Mar-1993'},{'id':2, 'name':'Amani', 'phone':'9951554425','email':'amani.palla18@gmail.com','dob':'24-Mar-1993'},{'id':3, 'name':'Arun', 'phone':'9951338825','email':'arunkumar5@gmail.com','dob':'24-Mar-1993'}]
   return render_template('user_detail.html', items_sec=items)
